805a18f04af6456773768cb4e2604fa427f0b528
Peder Smith <smith.peder@gmail.com>
2018-02-22 20:17:06 +0100

    Update keybindings

--- zsh/keybindings.sh
+++ zsh/keybindings.sh
@@ -1,12 +1,4 @@
-# up
-	function up_widget() {
-		BUFFER="cd .."
-		zle accept-line
-	}
-	zle -N up_widget
-	bindkey "^k" up_widget
-
-# git
+# Git add all, commit and push
 	function git_prepare() {
 		if [ -n "$BUFFER" ];
 			then
@@ -23,7 +15,7 @@
 	zle -N git_prepare
 	bindkey "^g" git_prepare
 
-# home
+# Go to home
 	function goto_home() { 
 		BUFFER="cd ~/"$BUFFER
 		zle end-of-line
@@ -31,34 +23,3 @@
 	}
 	zle -N goto_home
 	bindkey "^h" goto_home
-
-# Edit and rerun
-	function edit_and_run() {
-		BUFFER="fc"
-		zle accept-line
-	}
-	zle -N edit_and_run
-	bindkey "^v" edit_and_run
-
-# LS
-	function ctrl_l() {
-		BUFFER="ls -la"
-		zle accept-line
-	}
-	zle -N ctrl_l
-	bindkey "^l" ctrl_l
-
-# Enter
-	function enter_line() {
-		zle accept-line
-	}
-	zle -N enter_line
-	bindkey "^o" enter_line
-
-# Sudo
-	function add_sudo() {
-		BUFFER="sudo "$BUFFER
-		zle end-of-line
-	}
-	zle -N add_sudo
-	bindkey "^s" add_sudo
