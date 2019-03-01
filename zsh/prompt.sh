f5679abab937717613255df1235993549dd881a7
Peder Smith <smith.peder@gmail.com>
2019-03-01 13:45:24 +0100

    Update prompt

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -3,19 +3,20 @@ colors
 
 # Full lineup of the promt
 set_prompt() {
-    # Path 
-    PS1="%{$fg[white]%}%{$reset_color%}% "
-      PS1+="%{$fg[blue]%}${PWD/#$HOME/~}%{$reset_color%}"
+    # Path
+    PS1="%{$fg[blue]%}${PWD/#$HOME/~}%{$reset_color%}"
+    
     # Git
     if git rev-parse --is-inside-work-tree 2> /dev/null | grep -q 'true' ; 
     then
-      PS1+='  '
+      PS1+="%{$fg[white]%} on %{$reset_color%}% "
+      PS1+="%{$fg[magenta]%} %{$reset_color%}% "
       # Branch 
-      PS1+="%{$fg[yellow]%}$(git rev-parse --abbrev-ref HEAD 2> /dev/null)%{$reset_color%}"
+      PS1+="%{$fg[magenta]%}$(git rev-parse --abbrev-ref HEAD 2> /dev/null)%{$reset_color%}"
       # Status
       if [ $(git status --short | wc -l) -gt 0 ]; 
       then 
-        PS1+="%{$fg[green]%}  $(git status --short | wc -l | awk '{$1=$1};1')%{$reset_color%}"
+        PS1+="%{$fg[green]%} [ $(git status --short | wc -l | awk '{$1=$1};1')]%{$reset_color%}"
       fi
     fi
     if [[ -z "${VIRTUAL_ENV}" ]]; then
@@ -24,9 +25,9 @@ set_prompt() {
       PS1+="%{$fg[white]%} in%{$reset_color%}"
         PS1+="%{$fg[green]%}  %{$reset_color%}% "
 
-    fi 
-    # White ] at the end
-    PS1+="%{$fg[white]%} %{$reset_color%}% "
+    fi
+    PS1+=" "
+    RPROMPT="%{$fg[yellow]%}%D{%L:%M:%S} %{$reset_color%}% "
 }
 
 precmd_functions+=set_prompt
