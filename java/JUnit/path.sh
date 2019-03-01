bffbbe7c57fe6a7b45a1715d5d891726aabe22a8
Peder Smith <smith.peder@gmail.com>
2019-03-01 02:09:14 +0100

    Add java conf

---
+++ java/JUnit/path.sh
@@ -0,0 +1,4 @@
+# Export classpath for the testrunners
+export CLASSPATH=.:$CLASSPATH:~/dotfiles/java/JUnit/junit-4.12.jar:~/dotfiles/java/JUnit/hamcrest-core-1.3.jar
+
+source ~/dotfiles/java/JUnit/testrunner.sh # TestRunner
---
+++ java/MySQL/path.sh
@@ -0,0 +1,2 @@
+# Export classoath for the mysql connection jar
+export CLASSPATH=.:$CLASSPATH:~/dotfiles/java/MySQL/mysql-connector-java-8.0.15.jar
