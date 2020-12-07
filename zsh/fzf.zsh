1c2f5f26a4fa7cca65083900cfd6810d65e5d968
Peder Smith <smith.peder@gmail.com>
2020-12-07 11:15:36 +0100

    Add search in pdf files

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -11,3 +11,17 @@ fi
 # Key bindings
 # ------------
 source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
+rga-fzf() {
+	RG_PREFIX="rga --files-with-matches"
+	local file
+	file="$(
+		FZF_DEFAULT_COMMAND="$RG_PREFIX '$1'" \
+			fzf --sort --preview="[[ ! -z {} ]] && rga --pretty --context 5 {q} {}" \
+				--phony -q "$1" \
+				--bind "change:reload:$RG_PREFIX {q}" \
+				--preview-window="70%:wrap"
+	)" &&
+	echo "opening $file" &&
+	xdg-open "$file"
+}
