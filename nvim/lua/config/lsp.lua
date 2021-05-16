7c84de3d844c37dcca19544c636473d131797643
Peder Smith <smith.peder@gmail.com>
2021-05-16 16:00:00 +0200

    Fix LSP

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,52 +1,32 @@
--- Your custom attach function for nvim-lspconfig goes here.
-local on_attach = function(client, bufnr)
-    require "lsp_signature".on_attach(client)
+local nvim_lsp = require('lspconfig')
+local utils = require("utils")
 
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
-    local function buf_set_option(...)
-        vim.api.nvim_buf_set_option(bufnr, ...)
-    end
+-- Go to definition
+utils.map("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>")
+utils.map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+utils.map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+utils.map('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+utils.map('n', 'gh', '<Cmd>lua vim.lsp.buf.hover()<CR>')
 
-    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-
-    -- Mappings.
-    local opts = {noremap = true, silent = true}
-    buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-    buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-    buf_set_keymap("n", "<leader>law", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-    buf_set_keymap("n", "<leader>lrw", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-    buf_set_keymap("n", "<leader>llw", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-    buf_set_keymap("n", "<leader>lt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-    buf_set_keymap("n", "<leader>lrn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    buf_set_keymap("n", "<leader>lrf", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    buf_set_keymap("n", "<leader>ld", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-    buf_set_keymap("n", "<leader>ll", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-    buf_set_keymap("n", "<leader>lca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+local on_attach = function(_, bufnr)
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 end
 
--- Get builtin LSP running
+
+-- lua LSP must be built and installed manually
 require("nlua.lsp.nvim").setup(
-    require("lspconfig"),
-    {
-        on_attach = on_attach
-    }
+    require("lspconfig"), { on_attach = on_attach }
 )
-
+--
 -- pip install -U jedi-language-server
-require "lspconfig".jedi_language_server.setup{}
-
+nvim_lsp.jedi_language_server.setup{ on_attach = on_attach }
+--
 -- npm install -g typescript typescript-language-server
-require "lspconfig".tsserver.setup {}
-
+nvim_lsp.tsserver.setup{ on_attach = on_attach }
+--
 -- npm install -g vim-language-server
-require'lspconfig'.vimls.setup{}
-
+nvim_lsp.vimls.setup{ on_attach = on_attach }
+--
 -- npm i -g bash-language-server
-require'lspconfig'.bashls.setup{}
+nvim_lsp.bashls.setup{ on_attach = on_attach }
