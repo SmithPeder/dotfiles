4a20e0b7bbb23bdd191828ffaebbdd9b1f3c495a
Peder Smith <smith@Peders-MacBook-Pro.local>
2026-02-18 15:29:08 +0100

    2026

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,85 +1,50 @@
-# ====ALIAS-CONFIGURATION====
-
-# Exa ls
-  alias ls="exa --all"
-  alias l="exa --long --all --header --git"
-  alias tree="exa --tree --level=3"
-
-# Vim
-  alias vim="nvim"
-
 # Go back
-  alias ..="cd .."
+alias ..="cd .."
 
 # Git add
-  alias ga="git add -A"
+alias ga="git add -A"
 
 # Git commit
-  alias gc="git commit"
+alias gc="git commit"
 
 # Git status
-  alias gs="git status"
+alias gs="git status"
 
 # Git push
-  alias gp="git push"
+alias gp="git push"
 
 # Git log
-  alias gl="git log --oneline"
+alias gl="git log --oneline"
 
 # Git log
-  alias gd="git diff"
+alias gd="git diff"
 
-# Git checkout master
-  # alias gcm="git checkout master"
-  alias gcm="git checkout main"
+# Git checkout main
+alias gcm="git checkout main"
 
 # Git checkout new branch
-  alias gcn="git checkout -b"
-
-# Git branch
-  alias gb="git branch"
+alias gcn="git checkout -b"
 
 # Django runserver
-  alias runserver="./manage.py runserver"
+alias runserver="./manage.py runserver"
 
 # Django make migrations
-  alias makemigrations="./manage.py makemigrations"
+alias makemigrations="./manage.py makemigrations"
 
 # Django migrate
-  alias migrate="./manage.py migrate"
+alias migrate="./manage.py migrate"
 
 # Shell pluss
-  alias shell="./manage.py shell_plus"
-
-# Cat with color
-  alias cat="ccat"
-
-# Docker ps
-  alias ds="docker ps --format 'table {{.ID}}\\t{{.Names}}\t{{.Status}}'"
-
-# Docker-compose
-  alias d-c="docker-compose"
-
-# Ansible
-  alias ap="ansible-playbook"
-  alias av="ansible-vault"
+alias shell="./manage.py shell_plus"
 
 # Go to dotfiles
-  alias dotfiles="cd ~/dotfiles"
-
-# Take json from clipboard, fix and paste back
-  alias jt="pbpaste | jq '.' | pbcopy"
+alias dotfiles="cd ~/code/dotfiles"
 
 # Jump to tienda
-  alias tienda="cd ~/code/oda/tienda"
-  alias t="cd ~/code/oda/tienda"
-  alias web="cd ~/code/oda/tienda-web"
+alias tienda="cd ~/code/tienda"
+alias web="cd ~/code/tienda-web"
+alias hub="cd ~/code/hub"
+alias camino="cd ~/code/camino"
 
-  alias mat='echo "\e[0;31mNY28:\033[0m" && curl -s https://portal.ny28.no/wp-content/uploads/meny28.pdf | pdftotext - - && echo "\e[0;31mNY24:\033[0m" && curl -s https://www.ny24.no/api/fetch/$(date "+%u") | jq'
-
-  alias fixshell="stty sane"
-
-  alias deskbooker='~/code/oda/deskbooker/.venv/bin/python ~/code/oda/deskbooker/deskbooker/deskbooker.py'
-
-# Rebase with master
-  alias grom='git fetch && git rebase origin/main'
+# Klipy
+alias klipy="~/code/klipy/.venv/bin/python -m klipy"
